1f73c5a5c647ac764978d3c0e26a6c0b1bdb0f15
Austin <l02162010@gmail.com>
2017-01-10 15:57:06 +0800

    更改前端router

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,5 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.use('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 module.exports = router;
